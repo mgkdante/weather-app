60558c5b31dc66e1d95076ec7b6cd456d74c0b49
yesid <mgkdante@pm.me>
2024-05-25 20:54:40 -0400

    Added a working UI to display current weather

--- src/index.js
+++ src/index.js
@@ -2,10 +2,53 @@ import "./style.css";
 import { getAllData } from "./data";
 
 const searchCity = document.getElementById("search-city");
+const conditionImg = document.getElementById("current-condition-img");
+const currentCondition = document.getElementById("current-condition");
+const currentTemperature = document.getElementById("current-temperature");
+const currentHumidity = document.getElementById("current-humidity");
+const currentUV = document.getElementById("current-uv");
+const currentFeelsLike = document.getElementById("current-feels-like");
+const currentLastUpdated = document.getElementById("current-last-updated");
+const unitToggle = document.getElementById("unit-toggle");
+
+let currentData = null;
+
+const searchData = async (city) => {
+  currentData = await getAllData(city).current;
+  updateUI(currentData);
+  console.log(currentData);
+};
 
 searchCity.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    console.log(getAllData(searchCity.value));
+    searchData(searchCity.value);
+  }
+});
+
+const updateUI = (data) => {
+  conditionImg.src = data.conditionIcon;
+  currentCondition.textContent = data.condition;
+  currentHumidity.textContent = `Humidity: ${data.humidity}%`;
+  currentUV.textContent = `UV: ${data.uv}`;
+  currentLastUpdated.textContent = `Last Updated: ${data.lastUpdated}`;
+  if (unitToggle.checked === false) {
+    currentTemperature.textContent = `${data.tempC}°C`;
+    currentFeelsLike.textContent = `Feels like ${data.feelsLikeC}°C`;
+  } else {
+    currentTemperature.textContent = `${data.tempF}°F`;
+    currentFeelsLike.textContent = `Feels like ${data.feelsLikeF}°F`;
+  }
+};
+
+// Display weather data for Montreal by default on page load
+document.addEventListener("DOMContentLoaded", () => {
+  searchCity.value = "Montreal";
+  searchData("Montreal");
+});
+
+unitToggle.addEventListener("click", () => {
+  if (currentData) {
+    updateUI(currentData);
   }
 });
